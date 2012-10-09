164bd5bf65377ec4b0483ffc69a892b6f868ffe8
Kronos <vulcainus@gmail.com>
2012-10-09 13:02:42 -0300

    Updated License text & enabled meteors.

--- com/github/StormTeam/Storm/Storm.java
+++ com/github/StormTeam/Storm/Storm.java
@@ -1,19 +1,20 @@
-/*
+/**
  * Storm
  * Copyright (C) 2012 Icyene, Thidox
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package com.github.StormTeam.Storm;
 
@@ -90,7 +91,7 @@ public class Storm extends JavaPlugin {
             Lightning.load(this);
 //            Wildfire.load(this);
             Blizzard.load(this);
-//            Meteor.load(this);
+            Meteor.load(this);
             ThunderStorm.load(this);
 
         } catch (Exception e) {
