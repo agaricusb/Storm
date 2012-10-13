342b25c4158ceeb6e2ed93b8037825bef45c7a1e
Tudor <vulcainus@gmail.com>
2012-10-13 11:50:17 -0400

    Implemented ErrorLogger hack. Errors are now displayed better and stored to file.

---
+++ com/github/StormTeam/Storm/ErrorLogger.java
@@ -0,0 +1,101 @@
+package com.github.StormTeam.Storm;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginLogger;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.concurrent.Semaphore;
+import java.util.logging.LogRecord;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.lang.System.getProperty;
+
+public class ErrorLogger extends PluginLogger {
+
+    String PLUGIN_NAME = "";
+    String REGEX = "Task \\#\\d+ for Storm v[\\d.\\w]+ generated an exception";
+    Pattern ERROR_REGEX = Pattern.compile(REGEX.toLowerCase());
+    Plugin PLUGIN;
+
+    public ErrorLogger(Plugin context) {
+        super(context);
+        this.PLUGIN = context;
+        this.PLUGIN_NAME = PLUGIN.getName();
+    }
+
+    @Override
+    public void log(LogRecord logRecord) {
+        String generateErrorLog = "";
+        Matcher MATCHER = ERROR_REGEX.matcher(logRecord.getMessage().toLowerCase());
+        if (MATCHER.find()) {
+            generateErrorLog(logRecord);
+        } else {
+            super.log(logRecord);
+        }
+    }
+
+    private void generateErrorLog(LogRecord record) {
+        PluginDescriptionFile pdf = PLUGIN.getDescription();
+        Server server = Bukkit.getServer();
+        String error = "", name = PLUGIN_NAME + "_" + System.nanoTime() / 100000000 + ".error.log";
+        StringBuilder err = new StringBuilder();
+        boolean disable = false;
+
+        File root = new File(PLUGIN.getDataFolder(), "errors");
+        if (!root.exists())
+            root.mkdir();
+        File dump = new File(root.getAbsoluteFile(), name);
+
+        err.append("\n=============Storm has encountered an error!=============");
+        err.append("\nStacktrace:\n");
+        err.append((error = getStackTrace(record.getThrown())));
+        err.append("\nStorm version: " + pdf.getVersion());
+        err.append("\nPlugins loaded: " + Arrays.asList(server.getPluginManager().getPlugins()));
+        err.append("\nCraftBukkit version: " + server.getBukkitVersion());
+        err.append("\nStorm detected MC version: " + Storm.version);
+        err.append("\nJava version: " + getProperty("java.version"));
+        err.append("\nOS info: " + getProperty("os.arch") + " " + getProperty("os.name") + ", " + getProperty("os.version"));
+        err.append("\nPlease report this error to the Storm ticket tracker (http://github.com/StormTeam/Storm/issues)!");
+        error = error.toLowerCase();
+        if (error.contains("nullpointerexception") || error.contains("stackoverflowexception")) {
+            err.append("\nA critical error has been thrown. " + PLUGIN_NAME + " has been disabled to prevent further damage.");
+            disable = true;
+        } else {
+            err.append("\nError was minor; " + PLUGIN_NAME + " will continue operating.");
+        }
+        err.append("\nThis has been saved to the file ./" + PLUGIN.getName() + "/errors/" + name);
+        err.append("\n==========================================================");
+
+        System.out.println(err);
+
+        if (!dump.exists()) {
+            try {
+                Semaphore mutex = new Semaphore(1);
+                mutex.acquire();
+                dump.createNewFile();
+                BufferedWriter writer = new BufferedWriter(new FileWriter(dump));
+                mutex.release();
+                writer.write(err.toString());
+                writer.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+                //Do nothing! If you do e.printStackTrace, StackOverflow will ensue!
+            }
+        }
+
+        if (disable)
+            ((Storm) PLUGIN).disable();
+    }
+
+    private String getStackTrace(Throwable aThrowable) {
+        Writer result = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(result);
+        aThrowable.printStackTrace(printWriter);
+        return result.toString();
+    }
+}
