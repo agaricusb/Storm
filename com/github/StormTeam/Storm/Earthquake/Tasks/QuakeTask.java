e389b656e6844b23e45f0aef5dd31ec2cfc6971a
Xiaomao Chen <xiaomao5@live.com>
2012-11-13 18:03:20 -0500

    Formatted QuakeTask.

--- com/github/StormTeam/Storm/Earthquake/Tasks/QuakeTask.java
+++ com/github/StormTeam/Storm/Earthquake/Tasks/QuakeTask.java
@@ -6,35 +6,34 @@ import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
-import java.util.List;
-
 /**
+ * Task to shake every non-creative player
+ *
  * @author Giant
  */
 public class QuakeTask implements Runnable {
 
-    private Quake q;
+    private Quake quake;
     private boolean toggle;
 
     public QuakeTask(Quake q) {
-        this.q = q;
+        this.quake = q;
     }
 
 
     @Override
     public void run() {
-        List<Player> players = q.getWorld().getPlayers();
-        for (Player p : players) {
+        for (Player p : quake.getWorld().getPlayers()) {
             // Don't bother creative players
             if (p.getGameMode() == GameMode.CREATIVE)
                 continue;
 
             // Player isn't quaking...
-            if (!q.isQuaking(p.getLocation()))
+            if (!quake.isQuaking(p.getLocation()))
                 continue;
 
-            int x = q.getEpicenter().LEFT - p.getLocation().getBlockX() + 1;
-            int z = q.getEpicenter().RIGHT - p.getLocation().getBlockZ() + 1;
+            int x = quake.getEpicenter().LEFT - p.getLocation().getBlockX() + 1;
+            int z = quake.getEpicenter().RIGHT - p.getLocation().getBlockZ() + 1;
 
             int a = 5 / ((x + z) / 2);
 
